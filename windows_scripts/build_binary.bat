ee574d7b161c128da7bb68324af00536d3172a0f
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2024-04-02 17:37:56 +0530

    Updated build script

--- windows_scripts/build_binary.bat
+++ windows_scripts/build_binary.bat
@@ -1,9 +1,13 @@
 @echo off
 echo Building for Windows with MSBuild...
 cd ../build/
-IF "%~1"=="-r" msbuild C_Parser.sln  /p:Configuration=Release;Platform=x64 GOTO end
-IF "%~1"=="-d" msbuild C_Parser.sln  /p:Configuration=Debug;Platform=x64 GOTO end
-IF "%~1"=="" msbuild C_Parser.sln  /p:Configuration=Debug;Platform=x64 GOTO end
+IF "%~1"=="-r" msbuild C_Parser.sln  /p:Configuration=Release;Platform=x64
+IF "%~1"=="-r" goto  end
+IF "%~1"=="-d" msbuild C_Parser.sln  /p:Configuration=Debug;Platform=x64
+IF "%~1"=="-d" goto  end
+IF "%~1"=="" msbuild C_Parser.sln  /p:Configuration=Debug;Platform=x64
+IF "%~1"=="" goto end
+:error
 echo Unrecognized flag %~1
 echo                   ~~~
 exit
