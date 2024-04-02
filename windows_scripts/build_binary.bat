44dc2bdacd81fd6dc85fdc6430807a677fdb9934
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2024-04-02 13:09:54 +0530

    CMake Updated

--- windows_scripts/build_binary.bat
+++ windows_scripts/build_binary.bat
@@ -1,4 +1,12 @@
 @echo off
-echo "Building for Windows with MSBuild..."
+echo Building for Windows with MSBuild...
 cd ../build/
-msbuild C_Parser.sln  /p:Configuration=Release;Platform=x64
+IF "%~1"=="-r" msbuild C_Parser.sln  /p:Configuration=Release;Platform=x64 GOTO end
+IF "%~1"=="-d" msbuild C_Parser.sln  /p:Configuration=Debug;Platform=x64 GOTO end
+IF "%~1"=="" msbuild C_Parser.sln  /p:Configuration=Debug;Platform=x64 GOTO end
+echo Unrecognized flag %~1
+echo                   ~~~
+exit
+:end
+echo Done Building for Windows with MSBuild...
+exit
