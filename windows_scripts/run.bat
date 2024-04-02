2cd7c74e9e8ac249b4933182dbf6d8ee00a428bc
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2024-04-02 17:38:08 +0530

    Updated run script

--- windows_scripts/run.bat
+++ windows_scripts/run.bat
@@ -1,4 +1,19 @@
 @echo off
 cd ../build
+IF "%~1"=="-r" goto release
+IF "%~1"=="-d" goto debug
+IF "%~1"=="" goto debug
+:error
+echo Unrecognized flag %~1
+echo                   ~~~
+exit
+:debug
+echo Running Debug executable...
+cd Debug
+example.exe
+exit
+:release
+echo Running Release executable...
 cd Release
 example.exe
+exit
